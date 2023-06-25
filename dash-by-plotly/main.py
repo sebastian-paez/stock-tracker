81da4abde450d32d6c1ec002f028aa0b8bd594a0
Sebastian <csebastianpaez@outlook.com>
2023-06-24 21:57:02 -0400

    Add submit button

--- dash-by-plotly/main.py
+++ dash-by-plotly/main.py
@@ -10,24 +10,45 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H1("Stock Tracker", style={"textAlign": "center"}),
     html.Div("Set Ticker: "),
-    dcc.Dropdown(id="stock-dropdown", options=stock_list, style={"width": "40%"}),
+    dcc.Dropdown(id="stock-dropdown", options=stock_list, style={"width": "40%", "margin-bottom": "30px"}),
     html.Div("Set Lower Limit: "),
-    dcc.Input(id="lower-input",type="number", min=0, max=current_price),
+    dcc.Input(id="lower-input",type="number", min=0, max=current_price, style={"margin-bottom": "30px"}),
     html.Div("Set Upper Limit: "),
-    dcc.Input(id="upper-input", type="number", min=current_price, max=1000),
+    dcc.Input(id="upper-input", type="number", min=current_price, max=1000, style={"margin-bottom": "30px"}),
     html.Div("Current Price: "),
-    html.Div(id="current-price")
+    html.Div(id="current-price", style={"margin-bottom": "30px"}),
+    html.Button("Submit", id="submit-button", n_clicks=0),
 ])
 
+
+# Handle selected ticker
 @app.callback(
     Output("current-price", "children"),
-    Input("stock-dropdown", "value")
+    Output("lower-input", "max"),
+    Output("upper-input", "min"),
+    Input("stock-dropdown", "value"),
 )
-# Figure out how to update lower and upper limit bounds based on live price
 def update_values(stock_ticker):
     global current_price
     current_price = si.get_live_price(stock_ticker)
-    return current_price
+    return current_price, current_price, current_price
+
+
+# Handle submit button
+@app.callback(
+    Output("submit-button", "n_clicks"),
+    Output("stock-dropdown", "value"),
+    Output("lower-input", "value"),
+    Output("upper-input", "value"),
+    Input("submit-button", "n_clicks")
+)
+def submit_values(n_clicks):
+    if n_clicks != 0 or n_clicks != None:
+        # Add to tracked stocks here
+        # Figure out how to remove current price value when submitted
+        return None, None, None, None
+
+
 
 if __name__ == "__main__":
     app.run()
