5cba5f88f322ce096309375579d6d2e1ba242db9
Sebastian <csebastianpaez@outlook.com>
2023-06-26 23:46:20 -0400

    Add email notification functionality

--- dash-by-plotly/main.py
+++ dash-by-plotly/main.py
@@ -4,7 +4,6 @@ import config
 from yahoo_fin import stock_info as si
 from dash import Dash, html, dcc, Input, Output, State, callback
 from email.message import EmailMessage
-from datetime import datetime
 
 stock_list = si.tickers_dow()
 current_price = None
@@ -19,7 +18,7 @@ app.layout = html.Div(
     style={"display": "flex", "flexDirection": "column", "alignItems": "center", "justifyContent": "center", "fontFamily": "Helvetica"},
     children=[
     html.H1("Stock Tracker", style={"textAlign": "center"}),
-    dcc.Interval(id="trigger", interval=1000*10), # Updates every 10 seconds
+    dcc.Interval(id="trigger", interval=10000, n_intervals=0), # Updates every 10 seconds
     html.Div("Set Ticker: "),
     html.Div(dcc.Dropdown(id="stock-dropdown", options=stock_list, style={"width": "100px", "margin-bottom": "30px"})),
     html.Div("Set Lower Limit: "),
@@ -40,7 +39,8 @@ app.layout = html.Div(
             html.Div(children=[html.Div("Lower Limit: $" + str(lower)) for lower in tracked_lower]),
             html.Div(children=[html.Div("Upper Limit: $" + str(upper)) for upper in tracked_upper]),
         ]
-        )
+        ),
+    html.H3(id="placeholder", style={"margin-top": "30px"})
     ]
 )
 
@@ -53,6 +53,7 @@ def send_email(email, subject, body):
     msg.set_content(body)
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
     server.login(config.get_email(), config.get_password())
     server.send_message(msg)
     server.quit()
@@ -68,7 +69,7 @@ def send_email(email, subject, body):
 def update_values(stock_ticker):
     global current_price
     current_price = si.get_live_price(stock_ticker)
-    return "$" + str(round(current_price, 2)), current_price, current_price
+    return "$" + str(round(current_price, 2)), round(current_price, 2), round(current_price, 2)
 
 
 # Handle submit button
@@ -76,10 +77,10 @@ def update_values(stock_ticker):
     Output("submission-error", "children"),
     Output("tracked-stocks", "children"),
     Input("submit-button", "n_clicks"),
-    Input("stock-dropdown", "value"),
-    Input("lower-input", "value"),
-    Input("upper-input", "value"),
-    Input("email-address", "value")
+    State("stock-dropdown", "value"),
+    State("lower-input", "value"),
+    State("upper-input", "value"),
+    State("email-address", "value"),
 )
 def submit_values(n_clicks, stock_ticker, lower_limit, upper_limit, user_email):
     global num_clicks
@@ -94,12 +95,12 @@ def submit_values(n_clicks, stock_ticker, lower_limit, upper_limit, user_email):
             return html.Div("ERROR: This stock is already being tracked", style={"font-weight": "bold"}), [html.Div(children=[html.Div(stock) for stock in tracked_tickers]), 
                 html.Div(children=[html.Div("Lower Limit: $" + str(lower)) for lower in tracked_lower]), 
                 html.Div(children=[html.Div("Upper Limit: $" + str(upper)) for upper in tracked_upper])]
-        elif lower_limit == None or lower_limit >= current_price or lower_limit < 0:
-            return html.Div("ERROR: No lower limit", style={"font-weight": "bold"}), [html.Div(children=[html.Div(stock) for stock in tracked_tickers]), 
+        elif lower_limit == None or lower_limit > current_price or lower_limit < 0:
+            return html.Div("ERROR: Invalid lower limit", style={"font-weight": "bold"}), [html.Div(children=[html.Div(stock) for stock in tracked_tickers]), 
                 html.Div(children=[html.Div("Lower Limit: $" + str(lower)) for lower in tracked_lower]), 
                 html.Div(children=[html.Div("Upper Limit: $" + str(upper)) for upper in tracked_upper])]
-        elif upper_limit == None or upper_limit <= current_price:
-            return html.Div("ERROR: No upper limit", style={"font-weight": "bold"}), [html.Div(children=[html.Div(stock) for stock in tracked_tickers]), 
+        elif upper_limit == None or upper_limit < current_price:
+            return html.Div("ERROR: Invalid upper limit", style={"font-weight": "bold"}), [html.Div(children=[html.Div(stock) for stock in tracked_tickers]), 
                 html.Div(children=[html.Div("Lower Limit: $" + str(lower)) for lower in tracked_lower]), 
                 html.Div(children=[html.Div("Upper Limit: $" + str(upper)) for upper in tracked_upper])]
         else:
@@ -111,19 +112,32 @@ def submit_values(n_clicks, stock_ticker, lower_limit, upper_limit, user_email):
                 html.Div(children=[html.Div("Upper Limit: $" + str(upper)) for upper in tracked_upper])]
 
 
-# # Send email
-# @app.callback(
-#     Input("trigger", "n_intervals")
-# )
-# def check_current_price(_, ):
-#     i = 0
-#     for stock in tracked_tickers:
-#         if (si.get_live_price(stock) >= tracked_upper[i]):
-#             send_email(email_address, f"{stock} has reached the upper limit of {tracked_upper[i]}", f"Current price of {stock} is {si.get_live_price(stock)}")
-#         elif (si.get_live_price(stock) <= tracked_lower[i]):
-#             send_email(email_address, f"{stock} has reached the lower limit of {tracked_lower[i]}", f"Current price of {stock} is {si.get_live_price(stock)}")
-#         i += 1
-
+# Send email
+@app.callback(
+    Output("placeholder", "children"),
+    Input("trigger", "n_intervals"),
+    prevent_initial_call=True
+)
+def check_current_price(_):
+    global tracked_tickers
+    global tracked_lower
+    global tracked_upper
+    i = 0
+    for stock in tracked_tickers:
+        if (round(si.get_live_price(stock), 2) >= tracked_upper[i]):
+            send_email(email_address, f"{stock} has hit your threshold", f"{stock} has reached your upper limit and is trading at ${tracked_lower[i]} per share")
+            tracked_tickers.remove(stock)
+            tracked_lower.remove(tracked_lower[i])
+            tracked_upper.remove(tracked_upper[i])
+            return f"Upper Limit Reached for {stock}"
+        elif (round(si.get_live_price(stock), 2) <= tracked_lower[i]):
+            send_email(email_address, f"{stock} has hit your threshold", f"{stock} has reached your lower limit and is trading at ${tracked_lower[i]} per share")
+            tracked_tickers.remove(stock)
+            tracked_lower.remove(tracked_lower[i])
+            tracked_upper.remove(tracked_upper[i])
+            return f"Lower Limit Reached for {stock}"
+        i += 1
+    return ""
 
 
 if __name__ == "__main__":
